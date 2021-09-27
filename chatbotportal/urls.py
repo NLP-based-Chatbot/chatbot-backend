a4fccdda494b146cf77c71703092a428a6176119
Kavindu <kavinduravishka98sl@gmail.com>
2021-09-27 23:25:41 +0530

    created app chatbotportal + updated views and urls

--- chatbotportal/urls.py
+++ chatbotportal/urls.py
@@ -2,9 +2,9 @@ from django.conf.urls import url
 from django.urls import path, include, re_path
 from django.views.generic import TemplateView
 
+from . import views
+
 urlpatterns = [
-    path('auth/', include('djoser.urls')),
-    path('auth/', include('djoser.urls.jwt')),
-    path('assistant/',include('chatbotportal.urls'))
+    path('healthcare/',views.assistant_healthcare)
 ]
 
