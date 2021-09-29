f234ce6e3912610cde06d2a523c1bd37dba6fb22
Kavindu <kavinduravishka98sl@gmail.com>
2021-09-29 12:26:18 +0530

    created models

---
+++ healthcare/models.py
@@ -0,0 +1,49 @@
+from django.db import models
+
+# Create your models here.
+
+class Doctor(models.Model):
+    doctor_id   =   models.AutoField(primary_key=True)
+    initials    =   models.CharField(max_length=10)
+    first_name  =   models.CharField(max_length=30)
+    last_name   =   models.CharField(max_length=30)
+    docthash    =   models.CharField(max_length=40)
+
+
+class Appiontment(models.Model):
+    appointment_id  = models.AutoField(primary_key=True)
+    doctor_id       = models.IntegerField()
+    cust_id         = models.IntegerField()
+    time_slot       = Column(Time())
+    date            = Column(Date())
+
+class DoctorAvailable(models.Model):
+    avbl_id     = models.AutoField(primary_key=True)
+    doctor_id   = models.IntegerField()
+    date        = Column(Date())
+    time_from   = Column(Time())
+    time_to     = Column(Time())
+
+
+class DoctorSpec(models.Model):
+    d2s_id      = models.AutoField(primary_key=True)
+    doctor_id   = models.IntegerField()
+    spec_id     = models.IntegerField()
+
+
+class Specialization(models.Model):
+    spec_id     = models.AutoField(primary_key=True)
+    spec_name   = models.CharField(max_length=30)
+
+class Patient(models.Model):
+    cust_id     = models.AutoField(primary_key=True)
+    username    = models.CharField(max_length=100)
+    userhash    = models.CharField(max_length=40)
+
+class Report(models.Model):
+    report_id   = models.AutoField(primary_key=True)
+    cust_id     = models.IntegerField()
+    report_filename     = models.CharField(max_length=100)
+    reporthash  = models.CharField(max_length=40)
+
+    
