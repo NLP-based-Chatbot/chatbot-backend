6e22d6a7c79df61c1cdb07a8f9a917090a57fdd2
Kavindu <kavinduravishka98sl@gmail.com>
2021-09-29 16:41:35 +0530

    modified urls.py  and views.py in healthcare,transport,telecom

--- transport/views.py
+++ transport/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+import requests
+from django.http import JsonResponse,HttpResponse
+
+
+def runquery(request):
+    querydata=request.body
+    
 
 # Create your views here.
