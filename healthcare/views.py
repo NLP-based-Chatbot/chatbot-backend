8a5971507e692e3a3f8af2359b7ef3cd00959eae
Kavindu <kavinduravishka98sl@gmail.com>
2021-09-29 17:39:03 +0530

    temporary commit for helthcare app

--- healthcare/views.py
+++ healthcare/views.py
@@ -1,10 +1,16 @@
 from django.shortcuts import render
 import requests
 from django.http import JsonResponse,HttpResponse
+import json
 
 
 def runquery(request):
     querydata=request.body
+    querydata_dict = json.load(querydata)
+
+    
+
+
     
 
 # Create your views here.
