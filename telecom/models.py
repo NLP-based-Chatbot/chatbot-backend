4ca60db00e77d176f7650bce78fd9dd296715a21
Chandima Amarasena <chandimaamarasena.18@cse.mrt.ac.lk>
2021-10-03 02:20:24 +0530

    data packages api call implemented

--- telecom/models.py
+++ telecom/models.py
@@ -1,32 +1,9 @@
 from django.db import models
 
 class Packages(models.Model):
-    SERVICE_PROVIDER = [
-        ('DG', 'dialog'),
-        ('MT', 'mobitel'),
-        ('HT', 'hutch'),
-        ('AT', 'airtel')
-    ]
-
-    PAYMENT_METHOD = [
-        ('PR', 'prepaid'),
-        ('PO', 'postpaid')
-    ]
-
-    PACKAGE_TYPE = [
-        ('VC', 'voice'),
-        ('DT', 'data'),
-        ('UN', 'unlimited'),
-        ('AT', 'anytime'),
-        ('TB', 'time-based'),
-        ('CB', 'content-based'),
-        ('HB', 'home-broadband'),
-        ('TV', 'television')
-    ]
-
-    service_provider = models.CharField(max_length=2, choices=SERVICE_PROVIDER)
-    payment_method = models.CharField(max_length=2, choices=PAYMENT_METHOD )
-    package_type = models.CharField(max_length=2, choices=PACKAGE_TYPE)
+    service_provider = models.CharField(max_length=20)
+    payment_method = models.CharField(max_length=20)
+    package_type = models.CharField(max_length=20)
     package_name = models.CharField(max_length=255)
     value = models.IntegerField()
     description = models.CharField(max_length=255)
