45af1385173b49dc035562bd74316e967e5c2ff1
Chandima Amarasena <chandimaamarasena.18@cse.mrt.ac.lk>
2021-10-02 11:39:54 +0530

    Fixtures added

--- telecom/models.py
+++ telecom/models.py
@@ -16,13 +16,18 @@ class Packages(models.Model):
     PACKAGE_TYPE = [
         ('VC', 'voice'),
         ('DT', 'data'),
-        ('BB', 'broadband'),
+        ('UN', 'unlimited'),
+        ('AT', 'anytime'),
+        ('TB', 'time-based'),
+        ('CB', 'content-based'),
+        ('HB', 'home-broadband'),
         ('TV', 'television')
     ]
 
     service_provider = models.CharField(max_length=2, choices=SERVICE_PROVIDER)
     payment_method = models.CharField(max_length=2, choices=PAYMENT_METHOD )
     package_type = models.CharField(max_length=2, choices=PACKAGE_TYPE)
-    package_name = models.CharField(max_length=100)
+    package_name = models.CharField(max_length=255)
     value = models.IntegerField()
     description = models.CharField(max_length=255)
+    activation_method = models.CharField(max_length=255)
