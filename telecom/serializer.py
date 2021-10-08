424e275de192ca19c555c57d9d09bdffdb86f7d2
Chandima Amarasena <chandimaamarasena.18@cse.mrt.ac.lk>
2021-10-08 15:28:14 +0530

    Bug fixes

--- telecom/serializer.py
+++ telecom/serializer.py
@@ -3,4 +3,4 @@ from telecom.models import Packages
 class PackageSerializer(serializers.ModelSerializer):
     class Meta:
         model = Packages
-        fields = ['service_provider', 'payment_method', 'package_type', 'package_name', 'value', 'description']
+        fields = ['service_provider', 'payment_method', 'package_type', 'package_name', 'value', 'description', 'activation_method']
