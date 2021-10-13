08d1a3e59077ef6c6e180d63a0676e6a824fb921
Chandima Amarasena <chandimaamarasena.18@cse.mrt.ac.lk>
2021-10-14 01:06:06 +0530

    telecom, transport views protected

--- telecom/views.py
+++ telecom/views.py
@@ -1,9 +1,11 @@
+from rest_framework.decorators import api_view
 from telecom.serializer import PackageSerializer
 from telecom.models import Packages
 from django.shortcuts import render
 from django.http import JsonResponse,HttpResponse
 from rest_framework.parsers import JSONParser
 
+@api_view(['GET', 'POST'])
 def packages_view(request, service_provider, payment_method, package_type):
     if request.method == 'GET':
       snippets = Packages.objects.filter(service_provider=service_provider, payment_method=payment_method, package_type=package_type)
