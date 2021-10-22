a55bd4834de65b02d82bfcdfd99398adfbb294e6
Kavindu <kavinduravishka98sl@gmail.com>
2021-10-22 07:50:20 +0530

    test modified

--- healthcare/tests.py
+++ healthcare/tests.py
@@ -1,3 +1,53 @@
 from django.test import TestCase
 
-# Create your tests here.
+from rest_framework.test import APITestCase
+from rest_framework import status
+from healthcare.models import *
+import pdb
+
+class FeedbackTestCase(APITestCase):
+    def setUp(self):
+        self.register_data = {  "first_name" : "testuser", 
+                                "last_name" : "test",
+                                "email" : "test@gmail.com",
+                                "password" : "strongPassword123",
+                                "re_password": "strongPassword123" }
+        self.login_data = { "email" : "test@gmail.com",
+                            "password" : "strongPassword123"} 
+        
+        self.register_url = "/auth/users/"
+        self.login_url = "/auth/jwt/create/"
+
+        self.user_register()
+        self.user_login()
+        self.authentication()
+        
+        return super().setUp()
+    
+    def authentication(self):
+        self.access_code = self.login_response.data['access']
+        self.client.credentials(HTTP_AUTHORIZATION="JWT "+ self.access_code)
+
+    def user_register(self):
+        response = self.client.post(self.register_url, self.register_data)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.feedback_data['user_id'] = response.data['id']
+        email = response.data['email']
+        user = UserAccount.objects.get(email=email)
+        user.is_active = True
+        user.save()
+    
+    def user_login(self):
+        self.login_response = self.client.post(self.login_url, self.login_data)
+        self.assertEqual(self.login_response.status_code, status.HTTP_200_OK)
+
+    # def test_record_feedback(self):
+    #     response = self.client.post(self.feedback_url, self.feedback_data,format='json')
+    #     self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+    
+    # def test_get_feedback_data(self):
+    #     self.client.post(self.feedback_url, self.feedback_data, format='json')
+    #     response = self.client.get(self.feedback_url)
+    #     self.assertEqual(response.status_code, status.HTTP_200_OK)
+        
+    
