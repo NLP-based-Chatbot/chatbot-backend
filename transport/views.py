6c759a01610f9a71d78b15c586d00423527f92f3
YasithD <59837420+YasithD@users.noreply.github.com>
2021-11-03 10:20:50 +0530

    views changed

--- transport/views.py
+++ transport/views.py
@@ -6,7 +6,7 @@ from django.shortcuts import render
 from django.http import JsonResponse,HttpResponse
 from rest_framework.parsers import JSONParser
 
-@api_view(['GET', 'POST'])
+# @api_view(['GET', 'POST'])
 def schedule_view(request, vehical_type, departure, destination):
   if request.method == 'GET':
       print(departure, destination)
