78e31989df7aeed92ae738f63677c436bbfd6387
Chandima Amarasena <chandimaamarasena.18@cse.mrt.ac.lk>
2021-11-06 21:13:53 +0530

    Newsfeed app added

--- newsfeed/urls.py
+++ newsfeed/urls.py
@@ -7,5 +7,8 @@ urlpatterns = [
     path('news/domain/<str:domain>',csrf_exempt(views.news_list)),
     path('news/details/<str:pk>',csrf_exempt(views.news_detail)),
     path('news/',csrf_exempt(views.news_view)),
+    path('instructions/domain/<str:domain>',csrf_exempt(views.instructions_list)),
+    path('instructions/details/<str:pk>',csrf_exempt(views.instructions_detail)),
+    path('instructions/',csrf_exempt(views.instructions_view)),
 ]
 
